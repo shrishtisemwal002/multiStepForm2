1afc538bc7bebf2f43e8b34ce6221c3b0c1609b4
shrishtisemwal002 <shrishti.semwal@techsuperiors.com>
2024-04-20 21:25:53 +0530

    files uploaded

---
+++ assets/javascript/script.js
@@ -0,0 +1,249 @@
+let currentPage = 1;
+let totalSteps = document.querySelectorAll(".stepForm").length;
+
+let nextBtn = document.getElementById("nextBtn");
+let prevBtn = document.getElementById("prevBtn");
+
+let formData = {};
+
+function show(step) {
+    document.querySelectorAll(".stepForm").forEach(function (element) {
+        element.classList.remove("active");
+    });
+    document.getElementById("step" + step).classList.add("active");
+}
+
+function navigateForward(event) {
+    event.preventDefault();
+    nextBtn.disabled = true;
+
+    let currentForm = document.getElementById("step" + currentPage);
+    let inputFields = currentForm.querySelectorAll("input");
+    let gender = document.getElementById('gender').value;
+    isValid = true;
+
+    inputFields.forEach((input) => {
+        if (!isValidInput(input)) {
+            isValid = false;
+        }
+    });
+
+    if (isValid) {
+        inputFields.forEach(input => {
+            formData[input.placeholder] = input.value;
+        });
+
+        formData["Gender"] = gender;
+
+        localStorage.setItem("formData", JSON.stringify(formData));
+
+        if (currentPage === totalSteps) {
+
+            show(currentPage);
+            console.log("display");
+
+            document.getElementById("form").submit();
+            document.getElementById("form").reset();
+
+            window.location.href = "display.html";
+        }
+
+        if (currentPage < totalSteps) {
+            currentPage++;
+            show(currentPage);
+        }
+
+        if (currentPage != 1) {
+            prevBtn.disabled = false;
+        } else {
+            prevBtn.disabled = true;
+        }
+
+        if(currentPage == totalSteps) {
+            nextBtn.textContent = "Save";
+        } else {
+            nextBtn.textContent = "Save & Next";
+        }
+    }
+
+}
+
+function navigateBackward(event) {
+    event.preventDefault();
+    nextBtn.disabled = false;
+
+    if (currentPage > 1) {
+        currentPage--;
+        show(currentPage);
+    }
+    if (currentPage != 1) {
+        prevBtn.disabled = false;
+    } else {
+        prevBtn.disabled = true;
+    }
+    if(currentPage != totalSteps) {
+        nextBtn.textContent = "Save & Next";
+    }
+}
+
+function check() {
+
+    let currentSection = document.getElementById("step" + currentPage);
+    let inputFields = currentSection.querySelectorAll("input");
+    let gender = document.getElementById("gender").value;
+    let inputs = Array.from(inputFields);
+    let isEmpty = inputs.some(input => input.value.trim() === "");
+
+    let validGender = gender === "male" || gender === "female" || gender === "other";
+    console.log(validGender);
+
+    if (!isEmpty && validGender) {
+        nextBtn.disabled = false;
+    }
+    else {
+        nextBtn.disabled = true;
+    }
+
+}
+
+function validate(event, fieldName) {
+    let regexString = /^[A-Za-z ]+$/;
+    let regexNum = /^\d/;
+
+    switch (fieldName) {
+
+        case 'firstName': let regexFirstName = /^[A-Za-z]+$/;
+            validateField(event, regexFirstName);
+            break;
+
+        case 'lastName': let regexLastName = /^[A-Za-z]+$/;
+            validateField(event, regexLastName);
+            break;
+
+        case 'age':
+            validateField(event, regexNum);
+            break;
+
+        case 'phoneNumber': let regPhoneNumber = /^\d{1,10}/;
+            validateField(event, regPhoneNumber);
+            break;
+
+        case 'city': validateField(event, regexString);
+            break;
+
+        case 'country': validateField(event, regexString);
+            break;
+
+        case 'postalCode':
+            validateField(event, regexNum);
+            break;
+
+    }
+}
+
+function validateField(event, regex) {
+    let key = event.key;
+
+    if (key === 'Backspace') {
+        return false;
+    }
+    if (!regex.test(key)) {
+        event.preventDefault();
+        return true;
+    }
+
+    return false;
+}
+
+function isValidInput(input) {
+
+    let isValid = true;
+    let invalidPassword = document.querySelector('.invalidPassword');
+    let invalidConfirmPassword = document.querySelector('.invalidConfirmPassword');
+
+
+    switch (input.name) {
+        case "email":
+            let regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9-]+\.[a-zA-Z]{2,}$/;
+            isValid = regex.test(input.value);
+            break;
+
+        case "password":
+            let regexPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+            isValid = regexPassword.test(input.value);
+            break;
+
+        case "confirmPassword":
+            let password = document.getElementById('password').value.trim();
+            isValid = input.value === password;
+            break;
+
+        case "phoneNumber":
+            isValid = input.value.length === 10;
+            break;
+
+        case "postalCode":
+            isValid = input.value.length === 6;
+            break;
+
+        default:
+            isValid = true;
+
+    }
+
+
+    if ((input.name != 'confirmPassword') && (input.name != 'password')) {
+        if(!isValid) {
+            input.nextElementSibling.textContent = `Invalid ${input.placeholder}`;
+        }
+        else {
+            input.nextElementSibling.textContent = "";
+        }
+    } 
+
+    if (input.name === 'confirmPassword') {
+        if (!isValid) {
+            invalidConfirmPassword.innerText = "Password does not match";
+        } else {
+            invalidConfirmPassword.innerText = "";
+        }
+    }
+
+    if (input.name === 'password') {
+        if (!isValid) {
+            invalidPassword.innerText = "Invalid Password";
+        } else {
+            invalidPassword.innerText = "";
+        }
+
+
+    }
+
+    input.addEventListener('input', function () {
+        if (input.nextElementSibling && (input.nextElementSibling.classList.contains('confirmPswd') || input.nextElementSibling.classList.contains('pswd'))) {
+            return;
+        }
+        if(input.nextElementSibling) {
+            input.nextElementSibling.textContent = "";
+        }
+    });
+
+    return isValid;
+}
+
+function showPassword(input, iconId) {
+    let inputField = document.getElementById(input);
+    let icon = document.querySelector(`.${iconId} .fa-regular`);
+    console.log(icon);
+    if (inputField.type === "password") {
+        inputField.type = "text";
+        icon.classList.remove("fa-eye");
+        icon.classList.add("fa-eye-slash");
+
+    }
+    else {
+        inputField.type = "password";
+        icon.classList.remove("fa-eye-slash");
+        icon.classList.add("fa-eye");
+    }
+}
